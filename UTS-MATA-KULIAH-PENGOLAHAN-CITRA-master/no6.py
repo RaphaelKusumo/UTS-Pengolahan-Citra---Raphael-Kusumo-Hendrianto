ee18c6f598f9d9ef22e8cd88fff6c40b6f18e105
Raphael Kusumo Hendrianto <71378076+RaphaelKusumo@users.noreply.github.com>
2021-10-19 06:06:07 +0700

    Add files via upload

---
+++ UTS-MATA-KULIAH-PENGOLAHAN-CITRA-master/no6.py
@@ -0,0 +1,22 @@
+import stegano
+from PIL import Image, ImageChops
+from stegano import lsb, lsbset
+from stegano.steganalysis import statistics, parity
+import matplotlib.pylab as plt
+cover = Image.open('repo_file/Raphael.jpg').convert('RGB')
+stego = lsb.hide('repo_file/Raphael.jpg', 10*"LSB data hiding with Stegano").convert('RGB')
+stego.save('repo_file/Raphael.jpg')
+print(lsb.reveal('repo_file/Raphael.jpg'))
+parity_encoded_cover = parity.steganalyse(cover)
+parity_encoded_stego = parity.steganalyse(stego)
+_, cover_common = statistics.steganalyse(cover)
+_, stego_common = statistics.steganalyse(stego)
+
+plt.figure(figsize=(30,20))
+plt.subplot(231), plt.imshow(cover), plt.axis('off'), plt.title('Cover Image', size=20)
+plt.subplot(232), plt.imshow(stego), plt.axis('off'), plt.title('Stego Image', size=20)
+plt.subplot(233), plt.imshow(ImageChops.difference(stego, cover)), plt.axis('off'), plt.title('Diff Image', size=20)
+plt.subplot(234), plt.imshow(parity_encoded_cover), plt.axis('off'), plt.title('Parity Encoded Cover Image', size=20)
+plt.subplot(235), plt.imshow(parity_encoded_stego), plt.axis('off'), plt.title('Parity Encoded Stego Image', size=20)
+plt.subplot(236), plt.imshow(ImageChops.difference(parity_encoded_stego, parity_encoded_cover)), plt.axis('off'), plt.title('Diff in Parity Encoded Images', size=20)
+plt.show()
---
+++ UTS-MATA-KULIAH-PENGOLAHAN-CITRA-master/screenshoot/main.py
@@ -0,0 +1,2 @@
+import os
+HOME_FOLDER = 'D:\RAPHAEL\semester_7\Pengolahan_Citra\UTS-MATA-KULIAH-PENGOLAHAN-CITRA-master'
